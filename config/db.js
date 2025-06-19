9d1651a2aaec30ee9b4e13526f7c388d93154319
prince <princedossou465@gmail.com>
2025-06-19 15:01:36 +0100

    cors3

--- config/db.js
+++ config/db.js
@@ -1,8 +1,6 @@
-const mongoose =  require('mongoose')
-require('dotenv').config({path : './config/.env'})
-
-
+const mongoose = require('mongoose');
+require('dotenv').config({ path: './config/.env' });
 
 mongoose.connect(process.env.DB_USER_PASS)
-.then(()=>console.log('connection a mongodb'))
-.catch((err)=>console.log( err))
+  .then(() => console.log('✅ Connexion à MongoDB réussie'))
+  .catch((err) => console.error('❌ Erreur de connexion MongoDB :', err));
