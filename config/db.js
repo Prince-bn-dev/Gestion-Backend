c7ecfd7ba70feef4906536353bc93a39e3c98fff
prince <princedossou465@gmail.com>
2025-06-19 02:59:58 +0100

    vercel4

--- config/db.js
+++ config/db.js
@@ -3,7 +3,6 @@ require('dotenv').config({path : './config/.env'})
 
 
 
-mongoose.connect(
-'mongodb+srv://'+process.env.DB_USER_PASS+'@gestionvehicule.dnbj5rj.mongodb.net/gestionVehicule'
+mongoose.connect(process.env.DB_USER_PASS
 ).then(()=>console.log('connection a mongodb'))
 .catch((err)=>console.log( err))
