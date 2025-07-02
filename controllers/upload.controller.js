cefea087689a26a397ed420258a65ef29e6b4b8c
prince <princedossou465@gmail.com>
2025-07-02 10:16:21 +0100

    erreur image

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -5,16 +5,18 @@ const User = require('../models/User');
 
 module.exports.uploadProfil = async (req, res) => {
   try {
+    if (!req.file) throw new Error("no file");
+
     if (
       req.file.mimetype !== 'image/jpg' &&
       req.file.mimetype !== 'image/jpeg' &&
       req.file.mimetype !== 'image/png'
     ) {
-      throw Error('invalid file');
+      throw new Error("invalid file");
     }
 
     if (req.file.size > 500000) {
-      throw Error('max size');
+      throw new Error("max size");
     }
 
     const fileName = req.body.nom + '.jpg';
@@ -25,19 +27,24 @@ module.exports.uploadProfil = async (req, res) => {
     }
 
     fs.writeFileSync(path.join(uploadPath, fileName), req.file.buffer);
-     
+
     const user = await User.findByIdAndUpdate(
-        req.body.userId,
-        {$set:{image:"uploads/profil/"+fileName}},
-        {new:true , upsert:true, setDefaultsOnInsert:true}, 
-    )
-    return res.status(200).send({ message: 'Image enregistrée avec succès'+user });
+      req.body.userId,
+      { $set: { image: "uploads/profil/" + fileName } },
+      { new: true, upsert: true, setDefaultsOnInsert: true },
+    );
+
+    return res.status(200).send({ message: 'Image enregistrée avec succès', user });
+
   } catch (error) {
-    console.log("Erreur lors de l'enregistrement de l'image : ", error);
+    console.log("Erreur brute :", error);
+    console.log("Erreur message :", error.message);
+
     const err = uploadErrors(error);
     return res.status(400).send({ error: err });
-  };
+  }
 };
+
 
 
 
